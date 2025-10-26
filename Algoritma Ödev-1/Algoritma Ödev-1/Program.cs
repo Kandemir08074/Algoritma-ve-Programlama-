93c81e96de61776a686fc9c014759ef0155d381b
Mert Ali Kandemir <mertakandemir82@gmail.com>
2025-10-26 14:55:52 +0300

    Algoritma ve Programlama
    21 Ekim ders sonunda verilen ödev

---
+++ Algoritma Ödev-1/Algoritma Ödev-1/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace Algoritma_Ödev_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] dizi = new int[5, 3];
+            Random rnd = new Random();
+            Console.WriteLine("Üretilen matris: ");
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write("[");
+                for (int j = 0; j < 3; j++)
+                {
+                    dizi[i, j] = rnd.Next(0, 10);
+
+                    Console.Write(dizi[i, j]);
+                    if (j < 2)
+                    {
+                        Console.Write(",");
+                    }
+
+                }
+                Console.WriteLine("]");
+            }
+            Console.WriteLine("");
+            Console.WriteLine("Sütun Toplamları:");
+            Console.Write("Sütun 1: ");
+            int s1 = 0;
+            for (int i = 0; i < 5; i++)
+            {
+                s1 = s1 + dizi[i, 0];
+
+            }
+            Console.Write(s1);
+            Console.WriteLine("");
+            Console.Write("Sütun 2: ");
+            int s2 = 0;
+            for (int i = 0; i < 5; i++)
+            {
+                s2 = s2 + dizi[i, 1];
+            }
+            Console.Write(s2);
+            Console.WriteLine("");
+            Console.Write("Sütun 3: ");
+            int s3 = 0;
+            for (int i = 0; i < 5; i++)
+            {
+                s3 = s3 + dizi[i, 2];
+            }
+            Console.Write(s3);
+        }
+    }
+}
